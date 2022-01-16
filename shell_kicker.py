a09c55b3f956dc8d741716a319e59edff6622623
Scott Edwards <scotteza@gmail.com>
2022-01-16 20:48:09 +0000

    Handle non-200 response status codes and not-found divs

--- shell_kicker.py
+++ shell_kicker.py
@@ -10,13 +10,19 @@ with open('spreadsheet_id.txt', 'r') as file:
     spreadsheet_id = file.read()
 
 url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/"
-page = requests.get(url)
-entire_page = BeautifulSoup(page.text, "html.parser")
+response = requests.get(url)
+if response.status_code != 200:
+    print(f"Error, got response code {response.status_code}. Please check your spreadsheet ID. Exiting...")
+    quit()
+
+entire_page = BeautifulSoup(response.text, "html.parser")
 grid_div = entire_page.find(
     name="div", attrs={"id": "0-grid-table-container"})
+if grid_div == None:
+    print("Grid div not found. Exiting...")
+    quit()
 
 to_search_for = "boot"
-
 found = bool(grid_div.findAll(text=to_search_for))
 
 if found:
