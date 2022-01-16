edd559c5ab6e2dd03a47ab622714ac131e4b04e8
Scott Edwards <scotteza@gmail.com>
2022-01-16 18:52:47 +0000

    Fiddle with Beautiful Soup

--- twitter-kicker.py
+++ twitter-kicker.py
@@ -1,5 +1,12 @@
 from bs4 import BeautifulSoup
 import os
+import requests
 
-os.system('ls -l')
+page = requests.get("https://twitter.com/bob/with_replies")
+something = BeautifulSoup(page.text, "html.parser")
+something.find_all(name="div", attrs={"class":"jobsearch-SerpJobCard"})
+
+print(something)
+
+# os.system('ls -l')
 
